e415d1e0d594b25ece4b3592e6155f6105186791
Michael Falk <falk@humatects.de>
2022-03-23 17:29:01 +0100

    add windows service sample project (.NET framework)

---
+++ WindowsTestService/WindowsTestService/Service1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace WindowsTestService
+{
+    public partial class Service1 : ServiceBase
+    {
+        private int eventId = 1;
+
+        public Service1()
+        {
+            InitializeComponent();
+            eventLog1 = new System.Diagnostics.EventLog();
+            if (!System.Diagnostics.EventLog.SourceExists("MySource"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(
+                    "MySource", "MyNewLog");
+            }
+            eventLog1.Source = "MySource";
+            eventLog1.Log = "MyNewLog";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("In OnStart.");
+
+            // Set up a timer that triggers every minute.
+            Timer timer = new Timer();
+            timer.Interval = 60000; // 60 seconds
+            timer.Elapsed += new ElapsedEventHandler(this.OnTimer);
+            timer.Start();
+        }
+
+        private void OnTimer(object sender, ElapsedEventArgs e)
+        {
+            // TODO: Insert monitoring activities here.
+            eventLog1.WriteEntry("Monitoring the System", EventLogEntryType.Information, eventId++);
+        }
+
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("Stopped.");
+        }
+
+        protected override void OnContinue()
+{
+    eventLog1.WriteEntry("In OnContinue.");
+}
+
+    }
+}
